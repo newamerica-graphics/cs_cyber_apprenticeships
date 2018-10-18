11ca4f986f2dddec44243b5cc74264da95c9f4cc
lorenries <lorenries@users.noreply.github.com>
2018-10-18 10:43:16 -0400

    get chart title from data url

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const settings = {
         width={900}
         height={600}
         data={data.data}
-        title="Cybersecurity Apprenticeship Tracker"
+        title={data.meta[0].title}
       />,
       el
     );
