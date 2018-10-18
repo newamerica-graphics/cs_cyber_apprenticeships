c83284985c3d9685a17c1955cd036de63931f901
lorenries <lorenries@users.noreply.github.com>
2018-10-18 10:11:34 -0400

    change circle stroke/radius

--- src/charts/CustomMap/MapPin.js
+++ src/charts/CustomMap/MapPin.js
@@ -20,23 +20,29 @@ class MapPin extends React.Component {
     this.setState({ isActive });
   }
 
-  handleClick = (e) => {
+  handleClick = e => {
     const { d } = this.props;
     this.props.updateInfoPanel(d, d["lat"], d["lon"], true);
-  }
+  };
 
   render() {
     return (
       <circle
         cx={this.props.x}
         cy={this.props.y}
-        r={this.state.isActive || (this.props.activeLat === this.props.d["lat"] && this.props.activeLon === this.props.d["lon"]) ? 6 : 4}
+        r={5}
         onMouseEnter={this.handleMouseEnter.bind(this)}
         onMouseLeave={this.handleMouseLeave.bind(this)}
         onClick={this.handleClick.bind(this)}
         fill={this.props.d["Registration"] ? "#25D7B0" : "#F5A623"}
-        strokeWidth="2"
-        stroke="#F9F6F6"
+        strokeWidth={
+          this.state.isActive ||
+          (this.props.activeLat === this.props.d["lat"] &&
+            this.props.activeLon === this.props.d["lon"])
+            ? 2
+            : 1
+        }
+        stroke="#fff"
       />
     );
   }
